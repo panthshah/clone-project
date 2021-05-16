7accc717046a3c498f109e05aed4db99100e363e
VebCommerce <80314630+VebCommerce@users.noreply.github.com>
2021-05-16 21:41:18 +0530

    third commit

---
+++ components/Avatar.js
@@ -0,0 +1,14 @@
+function Avatar( { url, className }) {
+    return(
+        <img
+            loading="lazy"
+            
+            className={`h-6 rounded-full cursor-pointer transition duration-150
+             transform hover scale-110 ${className}`}
+            src={url}
+            alt="profile-picture"
+        />
+    );
+}
+
+export default Avatar;
---
+++ keys.js
@@ -0,0 +1,3 @@
+
+export const api_key='AIzaSyCMVwFy7x9CViPm4JYA8OWH8StijnuHzkY'
+export const context_key='80a6300c3d50e9a47'
